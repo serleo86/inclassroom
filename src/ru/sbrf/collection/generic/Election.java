03ede7be29b909d301318b0f39ea3d2bae621061
dianus <slpanichev.sbt@sberbank.ru>
2018-07-12 19:04:46 +0300

    Generic

---
+++ src/ru/sbrf/collection/generic/Election.java
@@ -0,0 +1,15 @@
+package ru.sbrf.collection.generic;
+
+public class Election {
+
+    enum Gander {
+        MALE, FEMALE
+    }
+
+    enum AgeCategory {
+        KID, YOUNG, MIDDLEAGE, PENSIONER
+    }
+
+
+
+}
